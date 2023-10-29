410f60424b9149bd0230c839144293ef9afadcb6
DevAdeelAhmad <devadeelahmad@gmail.com>
2023-10-30 04:08:01 +0500

    Dummy Expenses Showed and Styled

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -1,4 +1,6 @@
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { GlobalStyles } from "../../constants/styles";
+
 import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
 
@@ -27,14 +29,53 @@ const DUMMY_EXPENSES = [
     amount: 250.51,
     date: new Date("2023-12-22"),
   },
+  {
+    id: "e5",
+    description: "Ciggerate",
+    amount: 27.5,
+    date: new Date("2023-12-19"),
+  },
+  {
+    id: "e6",
+    description: "Lighter",
+    amount: 40,
+    date: new Date("2023-12-20"),
+  },
+  {
+    id: "e7",
+    description: "GoldLeaf",
+    amount: 30.5,
+    date: new Date("2023-12-21"),
+  },
+  {
+    id: "e8",
+    description: "Food",
+    amount: 250.51,
+    date: new Date("2023-12-22"),
+  },
+  {
+    id: "e9",
+    description: "Food",
+    amount: 250.51,
+    date: new Date("2023-12-22"),
+  },
 ];
 
 function ExpensesOuput({ expenses, expensesPeriod }) {
   return (
-    <View>
+    <View style={styles.container}>
       <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
       <ExpensesList expenses={DUMMY_EXPENSES} />
     </View>
   );
 }
 export default ExpensesOuput;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 24,
+    paddingTop: 24,
+    backgroundColor: GlobalStyles.colors.primary700,
+  },
+});
--- components/ExpensesOutput/ExpensesSummary.js
+++ components/ExpensesOutput/ExpensesSummary.js
@@ -1,4 +1,5 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import { GlobalStyles } from "../../constants/styles";
 
 function ExpensesSummary({ periodName, expenses }) {
   const expnsesSum = expenses.reduce((sum, expense) => {
@@ -6,10 +7,30 @@ function ExpensesSummary({ periodName, expenses }) {
   }, 0);
 
   return (
-    <View>
-      <Text>{periodName}</Text>
-      <Text>Rs. {expnsesSum.toFixed(2)}</Text>
+    <View style={styles.container}>
+      <Text style={styles.period}>{periodName}</Text>
+      <Text style={styles.sum}>Rs. {expnsesSum.toFixed(2)}</Text>
     </View>
   );
 }
 export default ExpensesSummary;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 8,
+    backgroundColor: GlobalStyles.colors.primary50,
+    borderRadius: 6,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  period: {
+    fontSize: 12,
+    color: GlobalStyles.colors.primary400,
+  },
+  sum: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: GlobalStyles.colors.primary500,
+  },
+});
